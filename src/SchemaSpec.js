61cb9759f8e712e9e108e98ec9517bfed2ae889c
Nicholas Beach <nicholascbeach@gmail.com>
2016-04-08 19:55:33 -0400

    Added tests and did associated refactoring and bug fixes

--- src/SchemaSpec.js
+++ src/SchemaSpec.js
@@ -10,13 +10,13 @@
 
 	var SchemaSpec = function(name) {
 		this.name = name;
-		this.validations = {};
-		this.universalValidations = [];
+		this._propertyConditions = {};
+		this._universalConditions = [];
 	};
 
 	SchemaSpec.prototype._addConditions = function(validationList, conditions) {
 		var list = [].concat(validationList);
-		if(SchemaSpec.validators.array(conditions)) {
+		if(SchemaSpec.conditions.array(conditions)) {
 			list = list.concat(conditions);
 		} else {
 			list.push(conditions);
@@ -25,16 +25,19 @@
 	};
 
 	SchemaSpec.prototype.all = function(conditions) {
-		this.universalValidations = this._addConditions(this.universalValidations, conditions);
+		this._universalConditions = this._addConditions(this._universalConditions, conditions);
 		return this;
 	};
 
 	SchemaSpec.prototype.property = function(name, conditions) {
-		if(SchemaSpec.validators.undefined(this.validations[name])) {
-			this.validations[name] = [];
+		if(SchemaSpec.conditions.undefined(this._propertyConditions[name])) {
+			this._propertyConditions[name] = [];
 		}
 
-		this.validations[name] = this._addConditions(this.validations[name], conditions);
+		if(!SchemaSpec.conditions.undefined(conditions) && !SchemaSpec.conditions.null(conditions)) {
+			this._propertyConditions[name] = this._addConditions(this._propertyConditions[name], conditions);
+		}
+
 		return this;
 	};
 
@@ -44,19 +47,15 @@
 	//};
 
 	SchemaSpec.prototype.validate = function(object) {
-		for (var property in this.validations) {
-			if(this._validateValue(this.validations[property], object[property]) === false) {
-				return false;
+		for (var propertyName in this._propertyConditions) {
+
+			for(var i = 0; i  < this._universalConditions.length; i++) {
+				if(this._universalConditions[i](object[propertyName]) === false) {
+					return false;
+				}
 			}
 
-		}
-
-		return true;
-	};
-
-	SchemaSpec.prototype._validateValue = function(validations, value) {
-		for(var property in validations) {
-			if (validations.hasOwnProperty(property) && validations[property](value) === false) {
+			if(this._validateValue(this._propertyConditions[propertyName], object[propertyName]) === false) {
 				return false;
 			}
 		}
@@ -64,22 +63,33 @@
 		return true;
 	};
 
+	SchemaSpec.prototype._validateValue = function(propertyConditions, value) {
+		for(var property in propertyConditions) {
+			if (propertyConditions.hasOwnProperty(property) && propertyConditions[property](value) === false) {
+				return false;
+			}
+		}
+
+		return true;
+	};
 
 
-	SchemaSpec.validators = (function(){
+
+
+	SchemaSpec.conditions = (function(){
 		var module = { min: {}, max: {}, not: {}, greater: {}, less: {} };
 
 		module.null = function(value) { return value === null; };
 		module.undefined = function(value) { return typeof value === "undefined"; };
-		module.string = function(value) { return typeof value === "string"; };
-		module.number = function(value) { return typeof value === "number"; };
-		module.boolean = function(value) { return typeof value === "boolean"; };
-		module.function = function(value) { return typeof value === "function"; };
-		module.object = function(value) { return typeof value === "object"; };
-		module.array = function(value) { return Object.prototype.toString.call(value) === '[object Array]'; };
+		module.strings = module.string = function(value) { return typeof value === "string"; };
+		module.numbers = module.number = function(value) { return typeof value === "number"; };
+		module.booleans = module.boolean = function(value) { return typeof value === "boolean"; };
+		module.functions = module.function = function(value) { return typeof value === "function"; };
+		module.objects = module.object = function(value) { return typeof value === "object"; };
+		module.arrays = module.array = function(value) { return Object.prototype.toString.call(value) === '[object Array]'; };
 
-		module.integer = function(number) { return module.number(number) && (number % 1) === 0; };
-		module.empty = function(value) { return module.string(value) && module.length(0)(value); };
+		module.integers = module.integer = function(number) { return module.number(number) && (number % 1) === 0; };
+		module.empty = function(value) { return module.string(value) && value.length === 0; };
 
 		//Generate not validators for function that don't utilize currying
 		var notValidators = {};
@@ -93,23 +103,23 @@
 		module.not = notValidators;
 
 		//Add currying validators
-		module.length = function(length) { return function(value) { return !module.not.undefined(value.length) && value.length === length }; };
-		module.equals = function(provided) { return function(value) { return value === provided }; };
-		module.min.length = function(minLength) { return function(value) { return !module.not.undefined(value.length) && value.length >= minLength }; };
-		module.max.length = function(maxLength) { return function(value) { return !module.not.undefined(value.length) && value.length <= maxLength }; };
-		module.min.value = function(minValue) { return function(value) { return value >= minValue }; };
+		module.length = function(length) { return function(value) { return module.not.undefined(value.length) && value.length === length }; };
+		module.not.length = function(length) { return function(value) { return module.not.undefined(value.length) && value.length !== length }; };
+		module.equal = {};
+		module.equal.to = function(provided) { return function(value) { return value === provided }; };
+		module.not.equal = {};
+		module.not.equal.to = function(provided) { return function(value) { return value !== provided }; };
+		module.min.length = function(minLength) { return function(value) { return module.not.undefined(value.length) && value.length >= minLength }; };
+		module.max.length = function(maxLength) { return function(value) { return module.not.undefined(value.length) && value.length <= maxLength }; };
 		module.greater.than = function(minValue) { return function(value) { return value > minValue }; };
-		module.max.value = function(maxValue) { return function(value) { return value <= maxValue }; };
 		module.less.than = function(maxValue) { return function(value) { return value < maxValue }; };
 
-		//TODO: combine with object validator?
+		//TODO: Finish and write tests for the conditions below
 		module.model = function(modelValidator) { return function(value) { modelValidator.validate(value); } };
 		module.arrayOf = function() { return Object.prototype.toString.call(value) === '[object Array]'; };
 
-
-
 		function evaluateConditions(conditions, value) {
-			if(SchemaSpec.validators.array(conditions)) {
+			if(SchemaSpec.conditions.array(conditions)) {
 				for(var i = 0; i < conditions.length; i++) {
 					if(conditions[i](value) === false) {
 						return false;
--- test/SchemaSpecTest.js
+++ test/SchemaSpecTest.js
@@ -1,57 +1,303 @@
 var expect = require('chai').expect;
-var Schema = require('../src/SchemaSpec');
+var SchemaSpec = require('../src/SchemaSpec');
 var is, are;
-are = is = Schema.validators;
+are = is = SchemaSpec.conditions;
 
 describe("SchemaSpec", function() {
+  var alwaysFails, alwaysPasses;
 
-  describe("validates", function() {
+  beforeEach(function() {
+    alwaysPasses = function() { return true; };
+    alwaysFails = function() { return false; };
+  });
 
-    describe("single object properties with", function() {
-      var object;
+  describe("validates objects with", function() {
 
-      beforeEach(function() {
-        object = { property: 5 };
+    describe("single conditions for a property and returns", function() {
+
+      it("true when the condition passes", function () {
+        var spec = new SchemaSpec()
+          .property('property', alwaysPasses);
+
+        expect(spec.validate({})).to.equal(true);
       });
 
-      it("single conditions", function () {
-        var validator = new Schema().property('property', is.not.string);
-        expect(validator.validate(object)).to.equal(true);
-      });
+      it("false when the condition fails", function () {
+        var spec = new SchemaSpec()
+          .property('property', alwaysFails);
 
-      it("multiple conditions", function () {
-        var validator = new Schema().property('property', [is.integer, is.min.value(10)]);
-        expect(validator.validate(object)).to.equal(false);
+        expect(spec.validate({})).to.equal(false);
       });
-
 
     });
 
-    describe("all object properties with", function() {
-      var object;
+    describe("multiple conditions for a property and returns", function() {
 
-      beforeEach(function() {
-        object = {
-          propertyA: 5,
-          propertyB: 10
-        };
+      it("true when all conditions pass", function () {
+        var spec = new SchemaSpec()
+          .property('property', [alwaysPasses, alwaysPasses]);
+
+        expect(spec.validate({})).to.equal(true);
       });
 
-      it("single conditions", function () {
-        var validator = new Schema()
-          .property('propertyA', is.not.undefined)
-          .property('propertyB', is.not.undefined)
-          .all(are.greater.than(40));
-        expect(validator.validate(object)).to.equal(true);
+      it("flase when at least one condition fails", function () {
+        var spec = new SchemaSpec()
+          .property('property', [alwaysPasses, alwaysFails]);
+
+        expect(spec.validate({})).to.equal(false);
+      })
+
+    });
+
+    describe("single conditions for all properties and returns", function() {
+
+      it("true when the condition passes", function () {
+        var spec = new SchemaSpec()
+          .property('property')
+          .all(alwaysPasses);
+
+        expect(spec.validate({})).to.equal(true);
       });
 
-      it("multiple conditions", function () {
-        var validator = new Schema().property('property', [is.integer, is.greater.than(10)]);
-        expect(validator.validate(object)).to.equal(false);
+      it("false when the condition fails", function () {
+        var spec = new SchemaSpec()
+          .property('property')
+          .all(alwaysFails);
+
+        expect(spec.validate({})).to.equal(false);
       });
 
+    });
+
+    describe("multiple conditions for all properties and returns", function() {
+
+      it("true when all conditions pass", function () {
+        var spec = new SchemaSpec()
+          .property('property')
+          .all([alwaysPasses, alwaysPasses]);
+
+        expect(spec.validate({})).to.equal(true);
+      });
+
+      it("false when at least one condition fails", function () {
+        var spec = new SchemaSpec()
+          .property('property')
+          .all([alwaysPasses, alwaysFails]);
+
+        expect(spec.validate({})).to.equal(false);
+      });
+
+    });
+
+    describe("multiple properties and returns", function() {
+
+      it("true when all properties conditions pass", function () {
+        var spec = new SchemaSpec()
+          .property('propertyA', alwaysPasses)
+          .property('propertyB', alwaysPasses);
+
+        expect(spec.validate({})).to.equal(true);
+      });
+
+      it("false when at least one property condition fails", function () {
+        var spec = new SchemaSpec()
+          .property('propertyA', alwaysPasses)
+          .property('propertyB', alwaysFails);
+
+        expect(spec.validate({})).to.equal(false);
+      });
 
     });
 
   });
+
+
+  describe('condition', function() {
+    var obj, spec;
+
+    function initObject() {
+      obj = {};
+      obj.undefined = undefined;
+      obj.null = null;
+      obj.empty = '';
+      obj.numbers = obj.number = 55.555;
+      obj.integers = obj.integer = 55;
+      obj.strings = obj.string = 'abc123';
+      obj.booleans = obj.boolean = true;
+      obj.arrays = obj.array = [1, 2, 3];
+      obj.objects = obj.object = { property: 1 } ;
+      obj.functions = obj.function = function(){};
+    }
+
+    beforeEach(function() {
+      spec = new SchemaSpec();
+      initObject();
+    });
+
+    //We can generate tests for the non-currying conditions
+    initObject();
+    Object.keys(obj).forEach(function(name) {
+
+      describe(name + ' returns', function() {
+
+        it('true when the value is a ' + name, function() {
+          var result = spec.property(name, is[name]).validate(obj);
+          expect(result).to.equal(true);
+        });
+
+        it('false when the value is not a ' + name, function() {
+          var otherValueName = name.indexOf('number') >= 0 ? 'object' : 'number' ;
+
+          var result = spec.property(otherValueName, is[name]).validate(obj);
+          expect(result).to.equal(false);
+        });
+
+      });
+
+      describe('not.'+ name + ' returns', function() {
+        var otherValueName = name.indexOf('number') >= 0 ? 'object' : 'number' ;
+
+        it('false when the value is a ' + name, function() {
+          var result = spec.property(otherValueName, is.not[name]).validate(obj);
+          expect(result).to.equal(true);
+        });
+
+        it('true when the value is not a ' + name, function() {
+          var result = spec.property(name, is.not[name]).validate(obj);
+          expect(result).to.equal(false);
+        });
+
+      });
+
+    });
+
+
+    describe('length returns', function() {
+
+      it('true when the value length matches the provided', function() {
+        var result = spec.property('string', is.length(6)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value length does not match the provided', function() {
+        var result = spec.property('string', is.length(3)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+    describe('not.length returns', function() {
+
+      it('true when the value does not match the provided', function() {
+        var result = spec.property('string', is.not.length(3)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value matches the provided', function() {
+        var result = spec.property('string', is.not.length(6)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+
+    describe('equal.to returns', function() {
+
+      it('true when the value matches the provided', function() {
+        var result = spec.property('string', is.equal.to('abc123')).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value does not match the provided', function() {
+        var result = spec.property('string', is.equal.to('xzy789')).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+    describe('not.equal.to returns', function() {
+
+      it('true when the value does not match the provided', function() {
+        var result = spec.property('string', is.not.equal.to('xzy789')).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value matches the provided', function() {
+        var result = spec.property('string', is.not.equal.to('abc123')).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+    describe('min.length returns', function() {
+
+      it('true when the value length is greater than the provided', function() {
+        var result = spec.property('string', is.min.length(5)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('true when the value length is equal to the provided', function() {
+        var result = spec.property('string', is.min.length(6)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value is less than the provided', function() {
+        var result = spec.property('string', is.min.length(7)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+    describe('max.length returns', function() {
+
+      it('true when the value length is less than the provided', function() {
+        var result = spec.property('string', is.max.length(7)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('true when the value length is equal to the provided', function() {
+        var result = spec.property('string', is.max.length(6)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value is greater than the provided', function() {
+        var result = spec.property('string', is.max.length(5)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+    describe('greater.than returns', function() {
+
+      it('true when the value length is greater than the provided', function() {
+        var result = spec.property('integer', is.greater.than(50)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value is less or equal to than the provided', function() {
+        var result = spec.property('string', is.greater.than(55)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+
+    describe('less.than returns', function() {
+
+      it('true when the value length is less than the provided', function() {
+        var result = spec.property('integer', is.less.than(60)).validate(obj);
+        expect(result).to.equal(true);
+      });
+
+      it('false when the value is greater than or equal to than the provided', function() {
+        var result = spec.property('string', is.less.than(55)).validate(obj);
+        expect(result).to.equal(false);
+      });
+
+    });
+
+
+
+  });
+
 });
